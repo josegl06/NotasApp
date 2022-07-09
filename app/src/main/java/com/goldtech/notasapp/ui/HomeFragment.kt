1f4054696dcab464ad87b4f884d5b277123bbb61
JoseG1990 <jose.gomez7@utp.ac.pa>
2022-07-09 15:01:09 -0500

    Se creo pantalla de home/menu

--- app/src/main/java/com/goldtech/notasapp/ui/HomeFragment.kt
+++ app/src/main/java/com/goldtech/notasapp/ui/HomeFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import com.goldtech.notasapp.R
 import com.google.firebase.auth.FirebaseAuth
@@ -15,11 +14,6 @@ import com.google.firebase.ktx.Firebase
 class HomeFragment : Fragment() {
 
     private lateinit var auth: FirebaseAuth
-
-    private lateinit var signUpLayout: LinearLayout
-    private lateinit var accountLayout: LinearLayout
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
